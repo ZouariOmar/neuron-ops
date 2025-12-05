88b1800eccff8fef12127eb796cec2867449da59
ZouariOmar <zouariomar20@gmail.com>
2025-12-05 21:43:48 +0100

    feat(blog): ✅ Add `Blog` page
    [DETAILS]
    - A src/app/pages/blog/*

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   { path: 'sectors', loadComponent: () => import('./pages/sectors/sectors').then(m => m.Sectors) },
   { path: 'ops-methodology', loadComponent: () => import('./pages/ops/ops').then(m => m.Ops) },
   { path: 'case-study', loadComponent: () => import('./pages/cases/cases').then(m => m.Cases) },
-  // { path: 'blog', loadComponent: () => import('./pages/blog/blog').then(m => m.Blog) },
+  { path: 'blog', loadComponent: () => import('./pages/blog/blog').then(m => m.Blog) },
   { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact) },
 ];
 
